0ed445df40ff3ec57b24aabf29e2e26cb7e64ed6
Mustafa TAÇYILDIZ <93773148+D1422-Mustafa@users.noreply.github.com>
2022-04-05 19:00:23 +0300

    proje react router ile yapıldı.

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link, NavLink } from "react-router-dom";
+import styles from "./Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <div className={styles.navbar}>
+
+    {/* <Link to ="/">Home</Link>  */}
+
+
+    <NavLink to ="/" className={({isActive})=> isActive ? styles.activeStyle : undefined}>Home</NavLink>
+    <NavLink to ="/about" className={({isActive})=> isActive ? styles.activeStyle : undefined}>About</NavLink>
+    <NavLink to ="/products" className={({isActive})=> isActive ? styles.activeStyle : undefined}>Products</NavLink>
+
+        {/* <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/products">Products</a> */}
+    </div>
+  )
+}
+
+export default Navbar;
